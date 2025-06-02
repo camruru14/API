630ab1eecc1cd92416af82f1f06a1ce7903939b8
camilarugamas <20230248@ricaldone.edu.sv>
2025-06-02 12:09:59 -0600

    Funciones para agregar pendientes

--- js/script.js
+++ js/script.js
@@ -33,3 +33,59 @@ function mostrarDatos(datos){
 }
 //llamada incial para que se carguen los datos que vienen del servidor
 obtenerPersona();
+
+
+//agregar nuevo registro
+
+const modal = document.getElementById("modal-agregar");
+const btnAgregar = document.getElementById("btnAbrirModal");
+const btncerrar = document.getElementById("btnCerrarModal");
+
+btnAgregar.addEventListener("click", () => {
+    modal.showModal(); //abrir el modal al hacr click
+});
+
+btncerrar.addEventListener("click", () => {
+    modal.close(); //Cerrar modal
+})
+
+//agregar nuevo integrante desde el formulario
+
+document.getElementById("frmAgregar").addEventListener("submit", async e => {
+    e.preventDefault();//e representa al evento submit, evita que el formulario se envie de golpe
+    //Capturar los valores del formulario
+    const nombre = document.getElementById("nombre").value.trim();
+    const apellido = document.getElementById("apellido").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const edad = document.getElementById("edad").value.trim();
+
+    //Validacion basica
+    if(!nombre || !apellido || !email || !edad){
+        alert("Complete todos los campos");
+        return;//Evitar que el formulario se envie
+    }
+
+    //llamar a la API
+    const respuesta = await fetch(API_url, {
+        method: "POST",
+        headers: {'Content-Type' :'application/json'},
+        body: JSON.stringify({nombre, apellido, email, edad})
+    });
+
+    if(respuesta.ok){
+        alert("El registro fue agregado correctamente");
+
+        //Limpiar el formulario y cerrar el modal
+        document.getElementById("frmAgregar").reset();
+
+        modal.close();
+
+        //recargar la tabla
+        obtenerPersona();
+    }
+
+    else{
+        alert("Hubo un error al agregar");
+    }
+
+});
